8907c1f5c58ed3d09b1fd5595202f626a944f302
jemeka2 <jemeka2@oberlin.edu>
2021-07-07 14:26:22 -0400

    First commit: Change Assignment

---
+++ src/ChangeAssignment.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class ChangeAssignment {
+    public static void main(String[] args)
+    {
+        Scanner keyboard = new Scanner(System.in);
+
+        double num1, num2, num3;
+
+        System.out.print( "First temperature? " );
+        num1 = keyboard.nextInt();
+        keyboard.nextLine();
+
+        System.out.print( "Second temperature? " );
+        num2 = keyboard.nextInt();
+        keyboard.nextLine();
+
+        //Averaging
+        num3 = (num1 + num2) / 2;
+
+        System.out.println("The max value is : " + Math.max(num1, num2));
+        System.out.println("The average value is : " + num3);
+
+    }
+}
